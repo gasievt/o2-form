027741bdc57b577a6edbe79c28f10089735d35c5
gasievt <gasievt@inbox.ru>
2024-09-12 16:04:30 +0300

    form php

--- form.php
+++ form.php
@@ -1,4 +1,24 @@
 <?php
-$formData = json_decode(file_get_contents('php://input'), true);
+$formData = [
+	"email" => null,
+	"phone" => null,
+	"phone-additional" => null,
+	"second-name" => null,
+	"name" => null,
+	"patronymics" => null,
+	"day-of-birth" => null,
+	"month-of-birth" => null,
+	"year-of-birth" => null,
+	"gender" => null
+];
 $errors = [];
-echo json_encode($formData);
+$formDataRequest = json_decode(file_get_contents('php://input'), true);
+if (isSameArrays(array_keys($formData), array_keys($formDataRequest))){
+	$errors = array_keys($formDataRequest, null);
+}
+file_put_contents('test.txt', print_r($errors, true), FILE_APPEND);
+function isSameArrays($a, $b){
+   sort($a);
+   sort($b);
+   return $a == $b;
+}

91bed911344050889d1ac78e885386a9aa628063
gasievt <gasievt@inbox.ru>
2024-09-13 15:48:31 +0300

    add php form validation

--- form.php
+++ form.php
@@ -1,29 +1,48 @@
 <?php
-$formData = [
-	"email" => null,
-	"phone" => null,
-	"phone-additional" => null,
-	"second-name" => null,
-	"name" => null,
-	"patronymics" => null,
-	"day-of-birth" => null,
-	"month-of-birth" => null,
-	"year-of-birth" => null,
-	"gender" => null
+const FORM_DATA = [
+	'email' => null,
+	'phone' => null,
+	'phone-additional' => null,
+	'second-name' => null,
+	'name' => null,
+	'patronymics' => null,
+	'day-of-birth' => null,
+	'month-of-birth' => null,
+	'year-of-birth' => null,
+	'gender' => null
 ];
 $errors = [];
 $formDataRequest = json_decode(file_get_contents('php://input'), true);
-if (isSameArrays(array_keys($formData), array_keys($formDataRequest))){
+if(json_last_error()){
+	$errors['json'] = 'bad json';
+	echo json_encode(['success' => 'false', 'errors' => $errors]);
+	exit();
+}
+if (isSameArrays(array_keys(FORM_DATA), array_keys($formDataRequest))){
 	$errors = array_keys($formDataRequest, null);
-}
-$errors = array_flip($errors);
-foreach($errors as $key => &$el){
-	if($key === 'phone-additional' || $key === 'patronymics'){
-		unset($errors[$key]);
-	}
+	$errors = array_flip($errors);
+	foreach ($errors as $key => &$el){
+		if($key === 'phone-additional' || $key === 'patronymics'){
+			unset($errors[$key]);
+		}
 	$el = 'Это поле обязательное для заполнения.';
+	} 
 }
-file_put_contents('test.txt', print_r($errors, true), FILE_APPEND);
+else {
+	$errors = array_diff(array_keys(FORM_DATA), array_keys($formDataRequest));
+	$errors = array_flip($errors);
+	foreach($errors as &$el){
+		$el = 'Это поле обязательно для заполнения';
+	}
+	echo json_encode(['success' => 'false', 'errors' => $errors]);
+	exit();
+}
+array_walk($formDataRequest, function(&$el){
+	$el = trim(strip_tags($el));
+});
+if (count($errors) === 0){
+	echo json_encode(['success' => 'true']);
+}
 function isSameArrays($a, $b){
    sort($a);
    sort($b);
